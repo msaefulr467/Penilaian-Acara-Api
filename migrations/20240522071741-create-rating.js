76a57347f9bf0570fa323569a0b34a44facfc306
Nama Anda <email@anda.com>
2024-05-22 14:23:26 +0700

    Konfigurasi Sequalize dan Implementasi Autentikasi JWT

---
+++ migrations/20240522071741-create-rating.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Ratings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      eventId: {
+        type: Sequelize.INTEGER
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      feedback: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Ratings');
+  }
+};
---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const express = require('express');
+    const { User } = require('../models');
+    const bcrypt = require('bcryptjs');
+    const jwt = require('jsonwebtoken');
+    const router = express.Router();
+
+    // Registrasi
+    router.post('/register', async (req, res) => {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ username, password: hashedPassword });
+        res.json(user);
+    });
+
+    // Login
+    router.post('/login', async (req, res) => {
+        const { username, password } = req.body;
+        const user = await User.findOne({ where: { username } });
+
+        if (user && await bcrypt.compare(password, user.password)) {
+            const token = jwt.sign({ id: user.id }, 'secret_key', { expiresIn: '1h' });
+            res.json({ token });
+        } else {
+            res.status(400).send('Username or password is incorrect');
+        }
+    });
+
+    module.exports = router;
